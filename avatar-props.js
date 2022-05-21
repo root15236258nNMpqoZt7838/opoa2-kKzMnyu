597a81b00940b6270c27e780fd2af87190e6ca0f
root15236258nNMpqoZt7838 <101230556+root15236258nNMpqoZt7838@users.noreply.github.com>
2022-05-21 16:11:25 -0300

    Create avatar-props.js

---
+++ avatar-props.js
@@ -0,0 +1,53 @@
+const props = [
+    [
+        base01 =  "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288ee1220af2b84459ad12a_01.svg",
+        base02 =  "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288e8541cc1464f09fea0f5_02.svg",
+        base03 =  "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288ee134d4bff076b9a3d75_03.svg",
+        base04 =  "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288ee137f4f99c2b00e99e5_04.svg",
+        base05 =  "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288ee138c4f040f24d8558e_05.svg"
+    ],
+    [
+        eyes01 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288efcad53df366eb4cf7df_01.svg",
+        eyes02 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288efcaede95030e509fd27_02.svg",
+        eyes03 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288e9a0df5c5932a6bbef7b_03.svg",
+        eyes04 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288efca18a3476bc0be0608_04.svg",
+        eyes05 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288efca1948ad4d07ad7012_05.svg",
+        eyes06 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288efca17f8bbed6f3d032c_06.svg"
+    ],
+    [
+        costume01 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a9e24d034eaee9e4a_01.svg",
+        costume02 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a5549af707997a8a1_02.svg",
+        costume03 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25aae2f9f9713abfd3f_03.svg",
+        costume04 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25ac801732d74276956_04.svg",
+        costume05 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a5d3bee7578b18f7d_05.svg",
+        costume06 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25aef00faa6db9b47df_06.svg",
+        costume07 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a7f4f99145a0eace0_07.svg",
+        costume08 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25aa920a9043aae6175_08.svg",
+        costume09 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a90c8d07d07c47c2f_09.svg",
+        costume10 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25b226efd0eb4855da6_10.svg",
+        costume11 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288e8da20af2bc44a9ab90f_11.svg",
+        costume12 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25aa920a9710bae6176_12.svg",
+        costume13 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25956b75c6c8b70ec89_13.svg",
+        costume14 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f2597f4f99fccc0eacde_14.svg",
+        costume15 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a6d725b85cf3624d0_15.svg",
+        costume16 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a4d4bff74739a553a_16.svg",
+        costume17 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25982360f3c2c160592_17.svg",
+        costume18 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f2591948ad8263ad7cf8_18.svg",
+        costume19 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25920af2b7cc89af0e2_19.svg",
+        costume20 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a7705fd680138e136_20.svg",
+        costume21 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f259a0062202a6729df1_21.svg",
+        costume22 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a90c8d0bf4ac47c2c_22.svg",
+        costume23 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a1948ad4698ad7d15_23.svg",
+        costume24 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288f25a3e6edaa0bcc7410f_24.svg"
+    ],
+    [
+        mouth01 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fe58ffb704d13302db_01.svg",
+        mouth02 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fef213c4293124de2b_02.svg",
+        mouth03 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fe17f8bb1dd83e8571_03.svg",
+        mouth04 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fe8c4f047e8dda1196_04.svg",
+        mouth05 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fed53df3532f4e919b_05.svg",
+        mouth06 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/6288ea5a58ffb78536315256_06.svg",
+        mouth07 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fedc0f4b85da67343f_07.svg",
+        mouth08 = "https://uploads-ssl.webflow.com/62798bc44b0fc710d9b5ad11/628930fe5efc21d389a0d679_08.svg"
+    ]
+]
